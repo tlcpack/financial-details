f9cc371c5662f4104719a4e3dd741044c3d9e327
Taylor Cooke <tcooke@relias.com>
2021-02-01 14:02:29 -0500

    showing data, having charts problems

---
+++ src/plugins/echarts.js
@@ -0,0 +1,7 @@
+import Vue from 'vue'
+import Echarts from 'vue-echarts'
+import * as charts from 'echarts/lib/echarts'
+// import 'echarts/lib/component/title'
+
+Vue.component('chart', Echarts)
+Vue.component('charts', charts)
